d43dc341edb8a702a5faefac3a3cd02588fff7d5
04myls <04myls@MacBook-Pro.local>
2023-06-30 01:36:21 +0900

    List.jsをClass Componentsに変更

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { List } from "./List";
 class App extends React.Component {
   render() {
-    return <div>クラスコンポーネントに変更</div>;
+    return (
+      <div>
+        クラスコンポーネントにしてみました。
+        <List title="取り扱い言語一覧" />
+      </div>
+    );
   }
 }
 export default App;
