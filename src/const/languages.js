4cf9f47b368960db9d84e1b682cc794fbe0707cd
04myls <04myls@MacBook-Pro.local>
2023-06-30 10:15:38 +0900

    componentDidMount の利用

--- src/const/languages.js
+++ src/const/languages.js
@@ -1,8 +1,9 @@
-export const LANGUAGES = [
-    'JavaScript',
-    'C++',
-    'Swift',
-    'Kotlin',
-    'PHP',
-    'Go'
-  ];
+const LANGUAGES = ["JavaScript", "C++", "Swift", "Kotlin", "PHP", "Go"];
+
+export const getLanguages = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(LANGUAGES);
+    }, 1000);
+  });
+};
